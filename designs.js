c7dd70f6fa62f0ec741ca8ec54d9137a4989b9f9
Vivek Singh <vivek13102308@gmail.com>
2017-09-09 06:26:54 +0530

    Project is done

--- designs.js
+++ designs.js
@@ -1,10 +1,26 @@
-// Select color input
-// Select size input
 
-// When size is submitted by the user, call makeGrid()
-
-function makeGrid() {
-
-// Your code goes here!
-
+function makeGrid(rows, columns) {
+	const N = rows;
+	const M = columns;
+	
+	for(let i = 1; i <= N; i++) {
+		$('#pixel_canvas').append('<tr></tr>');
+		for(let j = 1; j <= M; j++) {
+			$('tr').last().append('<td></td>');
+		}
+	}
 }
+$('form').submit(function(e) {
+	e.preventDefault();
+	let rows = $('#input_height').val();
+	let cols = $('#input_width').val();
+	$('tr').each(function() {
+		$(this).remove();
+	});
+	makeGrid(rows,cols);
+});
+
+$('table').on("click","td", function() {
+	let color = $('#colorPicker').val();
+	$(this).css("background-color", color);
+});
